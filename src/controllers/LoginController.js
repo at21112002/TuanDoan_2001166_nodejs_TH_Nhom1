7245e964a4b8236ad89edfc8f62a9920f8dbb487
phuchoang27 <hoangphuc092702@gmail.com>
2023-10-10 22:27:28 +0700

    Doan Nguyen Anh Tuan

---
+++ src/controllers/LoginController.js
@@ -0,0 +1,20 @@
+import express from "express"
+import userModel from './../services/userModel'
+const login = (req, res) => {
+    res.render('layout',{ data:{ title: 'Login', page:'Login' }});
+}
+const register = (req, res) => {
+    res.render('layout',{ data:{ title: 'Register', page:'Register' }});
+}
+const addRegister = async (req, res) => {
+    console.log(req.body)
+    let {fullname, user, password, address, sex, email} = req.body
+    let role = 'User'
+    await userModel.addNewUserRegiter(fullname, user, password, address, sex, email, role)
+    res.redirect("/login")
+}
+export default {
+login,
+register,
+addRegister
+}
